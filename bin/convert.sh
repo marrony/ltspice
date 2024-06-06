3f8f29cdcab605b74a4ad13ffdef95c103ff8084
Marrony Neris <neris.marrony@gmail.com>
2024-06-06 09:21:00 -0700

    convert to ASCII

--- bin/convert.sh
+++ bin/convert.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-if file "$1" | grep data > /dev/null; then
-  echo "converting $1"
-  iconv -f UTF16LE -t UTF8 < "$1" > "$1.new" && mv -f "$1.new" "$1"
+if file "$1" | grep UTF-8 > /dev/null; then
+  echo "converting UTF-8 to ASCII $1"
+  iconv -f UTF8 -t ISO-8859-2 < "$1" > "$1.new" && mv -f "$1.new" "$1"
+  exit 0
 fi
+
+if file "$1" | grep ISO-8859 > /dev/null; then
+  echo "converting ISO-8859 to ASCII $1"
+  iconv -f ISO-8859-1 -t ISO-8859-2 < "$1" > "$1.new" && mv -f "$1.new" "$1"
+  exit 0
+fi
+
+if file "$1" | grep data > /dev/null; then
+  echo "converting UTF-16-LE to ASCII $1"
+  iconv -f UTF16LE -t ISO-8859-2 < "$1" > "$1.new" && mv -f "$1.new" "$1"
+  exit 0
+fi
+
